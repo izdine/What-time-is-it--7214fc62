5baf989268752b453d542b6da0903100cf4f3724
Iz-Dine Amadou <2088346@talnet.nl>
2020-04-16 11:50:34 +0200

    Uitwerking van de exercise

--- script.js
+++ script.js
@@ -4,3 +4,6 @@ function myFunction() {
   document.getElementById("omtrek").innerHTML = mynumber.value *  Math.PI;
 document.getElementById("Oppervlakte").innerHTML = mynumber.value * mynumber.value * Math.PI * 0.25;
 }
+
+ var tijd = new Date();
+ document.getElementById("tijd").innerHTML = tijd;
